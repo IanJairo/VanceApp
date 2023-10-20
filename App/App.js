11269830f51636b7d03e4f360152cb88a9a8865d
Pericles Feitoza <periclesanfe@gmail.com>
2023-10-20 17:46:13 -0300

    TEST

--- App/App.js
+++ App/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { View, StyleSheet, Dimensions, Text, Image, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import MyTestScreen from './screens/MyTestScreen';
 import IntroScreen from './screens/introScreen'; 
 import PresentationScreenOne from './screens/presentationScreenOne'; 
@@ -12,13 +15,43 @@ import PasswordRegister from './screens/passwordRegisterScreen';
 import ResetPassword from './screens/passwordRecoveryScreen';
 import GeneratePin from './screens/generatePinScreen';
 import ValidatePin from './screens/validatePinScreen';
+import Config from './screens/configTabScreen';
+import Profile from './screens/profileTabScreen';
+import AboutUs from './screens/aboutUsTabScreen';
+
+import arrowImage from './assets/blackArrowIcon.png'
 
 const Stack = createStackNavigator();
+const Tab = createMaterialTopTabNavigator();
+const windowWidth = Dimensions.get('window').width;
+
+function BackButton({ navigation }) {
+  return (
+    <TouchableOpacity onPress={() => navigation.goBack()}>
+      <Image style={{
+        width: 18,
+        height: 18,
+        marginLeft: 20,
+      }} source={arrowImage}/>
+    </TouchableOpacity>
+  );
+}
+
+function ConfigTab() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Config" component={Config} />
+      <Tab.Screen name="About Us" component={AboutUs} />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
+  const navigation = useNavigation();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Intro">
+      <Stack.Navigator initialRouteName="ConfigTab">
         {/* Defina a rota para a tela de teste */}
         <Stack.Screen name="Intro" component={IntroScreen} />
         <Stack.Screen name="MyTest" component={MyTestScreen} />
@@ -31,6 +64,7 @@ export default function App() {
         <Stack.Screen name="ResetPassword" component={ResetPassword} />
         <Stack.Screen name='GeneratePin' component={GeneratePin} />
         <Stack.Screen name='ValidatePin' component={ValidatePin} />
+        <Stack.Screen name='ConfigTab' component={ConfigTab} options={{ headerTitle: (props) => <BackButton {...props} navigation={navigation} /> }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ App/screens/aboutUsTabScreen.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { View, Text, Image, Dimensions, TouchableOpacity} from 'react-native';
+import { StyleSheet } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+
+export default function AboutUs({ navigation }) {
+    useEffect(() => {
+        navigation.setOptions({
+          headerShown: false, // Esta opção oculta o cabeçalho da tela
+        });
+      }, []);
+
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#fff',        
+    },
+    
+});
