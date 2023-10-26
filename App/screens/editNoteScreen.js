f02d531716e9453c783ae2df682102ee4e252e6b
Pericles Feitoza <periclesanfe@gmail.com>
2023-10-26 09:38:58 -0300

    edit note screen modal share 1

--- App/screens/editNoteScreen.js
+++ App/screens/editNoteScreen.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, TextInput, Image, Modal } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, TextInput, Image, Modal , FlatList} from 'react-native';
 import RenderHTML from 'react-native-render-html';
 import { actions, RichToolbar ,RichEditor} from 'react-native-pell-rich-editor';
 import moment from 'moment';
 
+import testIcon from '../assets/profileIcon.png';
 import backArrow from '../assets/backArrow.png';
 import shareIcon from '../assets/shareIcon.png';
 import deletIcon from '../assets/deleteIcon.png';
@@ -11,19 +12,26 @@ import selectedStar from '../assets/selectedStar.png';
 import unselectedStar from '../assets/unselectedStar.png';
 
 const windowWidth = Dimensions.get('window').width*0.9;
-const windowHeight = Dimensions.get('window').height;
 
 export default function EditNote({ navigation }) {
+
     const richText = useRef();
 
     const [favorite, setFavorite] = useState(false);
     const [date, setDate] = useState('');
     const [title, setTitle] = useState('');
-    const [modalVisible, setModalVisible] = useState(false);
-
+    const [deletModalVisible, setDeletModalVisible] = useState(false);
+    const [sharedModalVisible, setSharedModalVisible] = useState(false);
+    const [toShareModalVisible, setToShareModalVisible] = useState(false);
 
     const [descHTML, setDescHTML] = useState('');
     const [showDescError, setShowDescError] = useState(false);
+    const [sharedUsers, setSharedUsers] = useState([
+      {name: 'Péricles', email: 'pericles@vance.com', foto: testIcon, permissao: 'true'},
+      {name: 'Ian', email: 'ian@vance.com', foto: testIcon, permissao: 'true'},
+      {name: 'Joao', email: 'joao@vance.com', foto: testIcon, permissao: 'false'},
+
+    ]);
 
     useEffect(() => {
         navigation.setOptions({
@@ -39,17 +47,22 @@ export default function EditNote({ navigation }) {
     }
 
     const handleDeleteNote = () => {
-      setModalVisible(true);
+      setDeletModalVisible(true);
     };
 
     const handleDeleteConfirm = () => {
       // Delete note logic here
-      setModalVisible(false);
+      setDeletModalVisible(false);
     };
   
     const handleDeleteCancel = () => {
-      setModalVisible(false);
+      setDeletModalVisible(false);
     };
+
+    const handleShareNote = () => {
+      setSharedModalVisible(!sharedModalVisible);
+    };
+
 
     const richTextHandle = (descriptionText) => {
         if (!descriptionText || title === '' ) {
@@ -76,7 +89,6 @@ export default function EditNote({ navigation }) {
     return (
       <SafeAreaView edges={["bottom", "left", "right"]} style={{ flex: 1 }}>
       <View style={{
-        opacity: modalVisible ? 0.5 : 1,
         flex: 1,
         height: "100%",
         backgroundColor: "white",
@@ -89,7 +101,7 @@ export default function EditNote({ navigation }) {
                     <TouchableOpacity onPress={() => navigation.navigate('Home')}><Image source={backArrow}/></TouchableOpacity>
                 </View>
                 <TouchableOpacity onPress={() => changeFavoriteHandle()}><Image style={styles.image} source={favorite ? selectedStar : unselectedStar}/></TouchableOpacity>
-                <TouchableOpacity><Image style={styles.image} source={shareIcon}/></TouchableOpacity>
+                <TouchableOpacity onPress={() => handleShareNote()}><Image style={styles.image} source={shareIcon}/></TouchableOpacity>
                 <TouchableOpacity onPress={() => handleDeleteNote()}><Image style={styles.image} source={deletIcon}/></TouchableOpacity>
             </View>
             <View style={{ flexDirection: 'row', alignItems: 'center' }}>
@@ -140,25 +152,67 @@ export default function EditNote({ navigation }) {
           <Text style={styles.textButtonStyle}>Save</Text>
         </TouchableOpacity>
       </View>
+
       <Modal
         animationType="slide"
         transparent={true}
-        visible={modalVisible}
+        visible={deletModalVisible}
         onRequestClose={() => {
-          setModalVisible(false);
+            setDeletModalVisible(false);
         }}
-      >
+        >
         <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.modalText}>Tem certeza?</Text>
+              <Text style={styles.modalText}>Ao confirmar, a assão não poderá ser desfeita, apenas refeita</Text>
+              <View style={styles.modalButtonsContainer}>
+                <TouchableOpacity style={styles.modalButtonNo} onPress={() => handleDeleteCancel()}>
+                    <Text style={styles.modalButtonTextNo}>Não, continuar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.modalButtonYes} onPress={() => handleDeleteConfirm()}>
+                    <Text style={styles.modalButtonTextYes}>Sim, cancelar</Text>
+                </TouchableOpacity>
+              </View> 
+            </View>
+        </View>
+    </Modal>
+
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={sharedModalVisible}
+      onRequestClose={() => {
+          setSharedModalVisible(false);
+      }}
+      >
+        <View style={styles.shareModalContainer}>
           <View style={styles.modalContent}>
-            <Text style={styles.modalText}>Tem certeza?</Text>
-            <Text style={styles.modalText}>Ao confirmar, a assão não poderá ser desfeita, apenas refeita</Text>
-            <View style={styles.modalButtonsContainer}>
-              <TouchableOpacity style={styles.modalButtonNo} onPress={() => handleDeleteCancel()}>
-                <Text style={styles.modalButtonTextNo}>Não, continuar</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.modalButtonYes} onPress={() => handleDeleteConfirm()}>
-                <Text style={styles.modalButtonTextYes}>Sim, cancelar</Text>
-              </TouchableOpacity>
+            <View style={styles.shareHeader}>
+              <TouchableOpacity style={{}} onPress={() => handleShareNote()}><Image source={backArrow} style={styles.image}/></TouchableOpacity>
+              <TouchableOpacity style={{}} onPress={() => handleChangeMoral()}><Text style={styles.linkText}>Adicionar</Text></TouchableOpacity>
+            </View>
+            <Text style={styles.h1}>Pessoas com acesso</Text>
+            <View style={styles.shareBody}>
+              <FlatList
+                data={sharedUsers}
+                renderItem={({item}) => (
+                  <View style={{ flexDirection: 'row', marginBottom: 15, alignItems: 'center', justifyContent: 'center'}}> 
+                    <Image source={item.foto} style={styles.image}/>
+                    <View style={{ flexDirection: 'column', marginRight: 10, width: windowWidth*0.4}}>
+                      <Text style={styles.h2}>{item.name}</Text>
+                      <Text style={styles.h3}>{item.email}</Text>
+                    </View>
+                    <View>
+                      <View style={{ flexDirection: 'row' }}>
+                        <TouchableOpacity style={{ height: 15, width: 15,marginLeft: 10,marginRight:5, borderRadius: 5, backgroundColor: item.permissao==='true' ? '#00c0ce' : '#7a7a7a' }}></TouchableOpacity>
+                        <Text style={styles.linkText1}>Editor</Text>
+                      </View>
+                    </View>
+                  </View>
+              )}
+              keyExtractor={item => item.email}
+              style={{ hidth: windowWidth*0.9, maxHeight: '90%', }}
+            />
             </View>
           </View>
         </View>
@@ -173,19 +227,17 @@ const styles = StyleSheet.create({
     fontWeight: "600",
     color: "#312921",
     marginBottom: 15,
-    width: windowWidth,
+    // width: windowWidth,
     height: '15%',
     justifyContent: 'space-between',
   },
-
   richTextContainer: {
     flex: 1,
     flexDirection: "column-reverse",
-    width: windowWidth,
+    // width: "100%",
     marginBottom: 10,
     height: '50%',
   },
-
   richTextEditorStyle: {
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
@@ -201,7 +253,6 @@ const styles = StyleSheet.create({
     elevation: 4,
     fontSize: 20,
   },
-
   richTextToolbarStyle: {
     backgroundColor: "#00c0ce",
     borderRadius: 10,
@@ -209,12 +260,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#f5f5f5",
   },
-
   errorTextStyle: {
     color: "#FF0000",
     marginBottom: 10,
   },
-
   saveButtonStyle: {
     backgroundColor: "#00c0ce",
     borderRadius: 10,
@@ -232,13 +281,11 @@ const styles = StyleSheet.create({
     elevation: 4,
     fontSize: 20,
   },
-
   textButtonStyle: {
     fontSize: 18,
     fontWeight: "600",
     color: "white",
   },
-
   image : {
     width: 25,
     height: 25,
@@ -248,7 +295,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '9e9e9e',
   },
   modalContent: {
     backgroundColor: 'white',
@@ -295,5 +341,39 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 16,
     textAlign: 'center',
+  },
+  shareModalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  shareHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: windowWidth*0.8,
+    height: '10%',
+  },
+  shareBody: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: windowWidth*0.8,
+    height: '90%',
+  },
+  h1: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#7a7a7a',
+    marginBottom: 20,
+  },
+  h2: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  h3: {
+    fontSize: 14,
+    color: '#7a7a7a',
   },
 });
